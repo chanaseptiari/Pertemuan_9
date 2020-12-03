93e9693aea72abec990ece2b905c3be4ddafe297
chanaseptiari <chanaseptiari@gmail.com>
2020-12-03 22:46:50 +0700

    Pertemuan 10

--- app/src/main/java/com/chana/pertemuan9/persegi.java
+++ app/src/main/java/com/chana/pertemuan9/persegi.java
@@ -3,21 +3,36 @@ package com.chana.pertemuan9;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class persegi extends AppCompatActivity {
-    EditText sisi;
+
+    EditText persegi;
     Button hitungPersegi;
-    TextView keliling,luas;
+    TextView kelilingPersegi,luasPersegi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_persegi);
 
-        sisi = findViewById(R.id.edit_persegi);
-        hitungPersegi = findViewById(R.id.btn_persegi);
+        persegi = findViewById(R.id.edit_persegi);
+        hitungPersegi = findViewById(R.id.btn_hitungpersegi);
+        kelilingPersegi = findViewById(R.id.kelilingPersegi);
+        luasPersegi = findViewById(R.id.luasPersegi);
 
+        hitungPersegi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int sisi = Integer.parseInt(persegi.getText().toString());
+                int keliling = sisi * 4;
+                int luas = sisi * sisi;
+                kelilingPersegi.setText(String.valueOf(keliling));
+                luasPersegi.setText(String.valueOf(luas));
+            }
+        });
     }
 }
--- app/src/main/java/com/chana/pertemuan9/persegipanjang.java
+++ app/src/main/java/com/chana/pertemuan9/persegipanjang.java
@@ -3,20 +3,37 @@ package com.chana.pertemuan9;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class persegipanjang extends AppCompatActivity {
 
-    EditText lebar,panjang;
+    EditText lebarPersegiPanjang,panjangPersegiPanjang;
     Button hitungPersegiPanjang;
-    TextView keliling,luas;
+    TextView kelilingPersegiPanjang,luasPersegiPanjang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_persegipanjang);
-        lebar = findViewById(R.id.edit_lebar);
+        lebarPersegiPanjang = findViewById(R.id.edit_lebar);
+        panjangPersegiPanjang = findViewById(R.id.edit_panjang);
+        hitungPersegiPanjang = findViewById(R.id.btn_persegipanjang);
+        kelilingPersegiPanjang = findViewById(R.id.kelilingPersegiPanjang);
+        luasPersegiPanjang = findViewById(R.id.luasPersegiPanjang);
+
+        hitungPersegiPanjang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int lebar = Integer.valueOf(lebarPersegiPanjang.getText().toString());
+                int panjang = Integer.valueOf(panjangPersegiPanjang.getText().toString());
+                int keliling = (panjang + lebar) * 2;
+                int luas = lebar * panjang;
+                kelilingPersegiPanjang.setText(String.valueOf(keliling));
+                luasPersegiPanjang.setText(String.valueOf(luas));
+            }
+        });
     }
 }
